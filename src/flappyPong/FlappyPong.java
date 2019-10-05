786078d4f2b6b722fa5c83473e4da4bb518c1063
cpebud <cpebud@aol.com>
2019-10-04 20:20:16 -0700

    First commit
    Change-Id: I434288c996bf38965840dbe6ac81031deb5856bb

---
+++ src/flappyPong/FlappyPong.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * File Name:		FlappyPong.java
+ * Project:			Flappy Pong Game
+ * 
+ * Designer:		Garrett Cross
+ * 
+ * Copyright © 2019 Garrett Cross. All rights reserved.
+ ******************************************************************************/
+package flappyPong;
+
+/**
+ * <tt>FlappyPong</tt>:
+ * A game based on a tutorial by 
+ * <A HREF="https://www.toptal.com/resume/oguz-gelal">Oguz Gelal</A>.
+ * <P>
+ * Tutorial found <A HREF="https://www.toptal.com/game/ultimate-guide-to-processing-simple-game">here</A>.
+ * <P>
+ * This game was made in Eclipse, and some changes have been made from
+ * the original design of the game. This is not an exact duplicate of 
+ * the tutorial game.
+ * 
+ * @author  Garrett Cross
+ * @version 0.0.1
+ * @since   10/04/2019
+ * 
+ */
+public class FlappyPong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
